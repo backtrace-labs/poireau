ab0abdea3bb27872f60abbe62d57275045acf8b8
Paul Khuong <pkhuong@backtrace.io>
2022-02-02 17:15:08 -0500

    poireau.sh: make it possible to pass arguments to poireau.py
    If the first argument is "*", it means we want system-wide profiling.
    Every remaining argument after the first argument, if any, is passed
    to poireau.py
    
    TESTED=manually.

--- scripts/poireau.sh
+++ scripts/poireau.sh
@@ -7,10 +7,15 @@ BASE=$(dirname $(readlink -f "$0"))
 if [ -z "$1" ]
 then
     TARGET="-a"
+elif [ "$1" = "*" ]
+then
+    TARGET="-a"
+    shift
 else
     TARGET="-p $1"
+    shift
 fi
 
 sudo ${PERF:-perf} trace -T $TARGET \
      -e sdt_libpoireau:* --call-graph=dwarf --sort-events 2>&1 |  \
-    "${BASE}/poireau.py"
+    "${BASE}/poireau.py" "$@"
