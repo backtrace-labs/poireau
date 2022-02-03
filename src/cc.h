ded7c65a167fad56d0ed9d1aad00f1f263f09bed
Paul Khuong <pvk@pvk.ca>
2022-02-03 10:22:45 -0500

    cc.h: restore build on clang
    As of clang 12, `noclone` isn't a thing.
    
    TESTED=by building with gcc-8 and clang-12.

--- src/cc.h
+++ src/cc.h
@@ -2,7 +2,12 @@
 
 /* SPDX-License-Identifier: MIT */
 
+#ifdef __clang__
+#define NOINLINE __attribute__((noinline))
+#else
 #define NOINLINE __attribute__((noinline, noclone))
+#endif /* !__clang__ */
+
 #define LIKELY(X) (__builtin_expect(!!(X), 1))
 #define UNLIKELY(X) (__builtin_expect(!!(X), 0))
 #define EXPORT __attribute__((visibility("default")))
